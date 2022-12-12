bc02c202942cdbc2300b4fe1824b5b9699879748
DonJackie <o12@kakao.com>
2022-12-12 14:35:38 +0900

    [마우스로 그림 그리기]상태를 직접 관리하는 방식

--- ExamWin32.cpp
+++ ExamWin32.cpp
@@ -1,36 +1,56 @@
-﻿//https://www.mathsisfun.com/algebra/trig-sin-cos-tan-graphs.html
-#include "pch.h"
+﻿#include "pch.h"
 #include "tipsware.h"
-#include <math.h>   // sin, cos 함수 사용!
 
-NOT_USE_MESSAGE; //이 프로그램은 특별한 메시지를 사용하지 않는다.
+struct AppData // 프로그램에서 사용할 내부 데이터
+{
+    POINT start_pos;  // 시작 점을 기억할 변수
+    char is_clicked;  // 마우스 클릭 여부를 저장할 변수 (1이면 클릭, 0이면 해제)
+};
+
+// 마우스 왼쪽 버튼이 눌러졌을 때 호출되는 함수를 정의한다!
+void OnLButtonDown(int a_mixed_key, POINT a_pos)
+{
+    AppData* p_data = (AppData*)GetAppData(); // 프로그램의 내부 데이터 주소를 가져온다.
+
+    if (p_data->is_clicked == 0) { // 마우스가 클릭이 안된 상태인지 체크한다.
+        p_data->is_clicked = 1;  // 마우스를 클릭 상태로 설정한다.
+        p_data->start_pos = a_pos; // 선 그리기의 시작 점을 저장한다.
+    }
+}
+
+// 마우스 왼쪽 버튼이 눌렀다가 해제 되었을 때 호출되는 함수를 정의한다!
+void OnLButtonUp(int a_mixed_key, POINT a_pos)
+{
+    AppData* p_data = (AppData*)GetAppData(); // 프로그램의 내부 데이터 주소를 가져온다.
+
+    if (p_data->is_clicked == 1) { // 마우스가 클릭된 상태인지 체크한다.
+        p_data->is_clicked = 0; // 마우스를 클릭 해제 상태로 설정한다.
+        Line(p_data->start_pos.x, p_data->start_pos.y, a_pos.x, a_pos.y);  // 시작 점과 끝점을 연결하여 선을 그린다.  
+        ShowDisplay(); // 정보를 윈도우에 출력한다.
+    }
+}
+
+// 마우스가 이동될 때마다 호출되는 함수를 정의한다!
+void OnMouseMove(int a_mixed_key, POINT a_pos)
+{
+    AppData* p_data = (AppData*)GetAppData(); // 프로그램의 내부 데이터 주소를 가져온다.
+
+    if (p_data->is_clicked == 1) {  // 마우스가 클릭된 상태인지 체크한다.
+        Line(p_data->start_pos.x, p_data->start_pos.y, a_pos.x, a_pos.y); // 시작 점과 끝점을 연결하여 선을 그린다.
+        p_data->start_pos = a_pos; // 현재 점을 다음 선의 시작 점으로 사용하기 위해 시작 점을 변경한다.
+        ShowDisplay(); // 정보를 윈도우에 출력한다.
+    }
+}
+
+// 마우스 왼쪽 버튼 누르거나, 해제하거나 이동할 때 호출될 함수들을 등록한다!
+MOUSE_MESSAGE(OnLButtonDown, OnLButtonUp, OnMouseMove)
 
 int main()
 {
-    // 회색 점선을 사용해서 X축과 Y축을 의미하는 선을 그린다!!
-    Line(0, 100, 720, 100, RGB(200, 200, 200), 1, PS_DOT);  // 수평선 (X축)
-    Line(360, 0, 360, 200);   // 수직선 (Y축)
+    AppData data = { {0, 0}, 0 };  // 프로그램이 내부적으로 사용할 메모리를 선언한다.
+    SetAppData(&data, sizeof(AppData)); // 지정한 변수를 내부 데이터로 저장한다.
 
-    SelectPenObject(RGB(0, 0, 255), 3);  // 3 굵기의 파랑색 선을 사용!
-    MoveTo(0, 100);  // sin 그래프의 시작 위치를 정한다.
-    int y;
-    for (int degree = 0; degree < 720; degree++) {   // 0도에서 720도까지 반복한다.
-        // degree에 해당하는 sin 값을 구하고 sin 값을 100배 증가시켜서 사용한다.
-        y = 100 - (int)(sin(degree * 3.141592 / 180) * 100);
-        // 기준점에서 (degree, y) 좌표까지 선을 그린다.
-        // 지금 그린 이 끝점이 다음 그리기의 기준점이 된다.
-        LineTo(degree, y);
-    }
-
-    SelectPenObject(RGB(255, 0, 0), 3);  // 3 굵기의 빨강색 선을 사용!
-    MoveTo(0, 0);  // cos 그래프의 시작 위치를 정한다.
-    for (int degree = 0; degree < 720; degree++) {  // 0도에서 720도까지 반복한다.
-        // degree에 해당하는 cos 값을 구하고 cos 값을 100배 증가시켜서 사용한다.
-        y = 100 - (int)(cos(degree * 3.141592 / 180) * 100);
-        // 기준점에서 (degree, y) 좌표까지 선을 그린다.
-        LineTo(degree, y);
-    }
-
+    SelectPenObject(RGB(0, 0, 255), 3);  // 파랑색이고 굵기가 3인 선 속성을 사용한다.
     ShowDisplay(); // 정보를 윈도우에 출력한다.
     return 0;
 }
