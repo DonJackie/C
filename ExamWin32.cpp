439597807bf0777b0ab70290f197b063f5f4d957
DonJackie <o12@kakao.com>
2022-12-13 15:41:10 +0900

    꼭 다시 봐야할 코드

--- ExamWin32.cpp
+++ ExamWin32.cpp
@@ -1,85 +1,41 @@
-﻿#include "pch.h"
+﻿//https://cafe.naver.com/easywin32/92
+#include "pch.h"
 #include "tipsware.h"
-
-#define MAX_COUNT   100   // 저장할 사각형의 최대 개수
-
-struct AppData // 프로그램에서 사용할 내부 데이터
-{
-    POINT start_pos;  // 시작 점을 기억할 변수
-    char is_clicked;  // 마우스 클릭 여부를 저장할 변수 (1이면 클릭, 0이면 해제)
-    int r_count; // 현재 저장한 사각형의 개수
-    RECT r_list[MAX_COUNT]; // 저장한 사각형 좌표 목록
-};
 
 // 마우스 왼쪽 버튼이 눌러졌을 때 호출되는 함수를 정의한다!
 void OnLButtonDown(int a_mixed_key, POINT a_pos)
 {
-    AppData* p_data = (AppData*)GetAppData(); // 프로그램의 내부 데이터 주소를 가져온다.
+    // 출력된 사각형은 일정한 크기를 가지기 때문에 사각형이 시작된 위치를 (0, 0)으로
+    // 계산되도록 마우스의 x 좌표는 20을 빼고 y 좌표는 60을 뺍니다. 그리고 사각형의
+    // 폭과 높이가 균일하게 60이기 때문에 마우스 좌표를 60으로 나누면 가로, 세로 60 크기의
+    // 사각형 인덱스를 구할 수 있습니다. 즉, 6개의 사각형 중에 첫 번째 사각형은 x가 0이고
+    // y도 0이며 두 번째 사각형은 x가 1이고 y가 0이 된다는 뜻입니다.
+    unsigned int x = (unsigned int)(a_pos.x - 20) / 60, y = (unsigned int)(a_pos.y - 60) / 60;
 
-    if (p_data->is_clicked == 0) { // 마우스가 클릭이 안된 상태인지 체크한다.
-        p_data->is_clicked = 1;  // 마우스를 클릭 상태로 설정한다.
-        p_data->start_pos = a_pos; // 새로운 사각형의 시작 점을 저장한다.
-    }
-}
-
-// 저장된 사각형들을 모두 다시 그리는 함수!
-void DrawRectList(AppData* ap_data)
-{
-    Clear();  // 화면을 지운다.
-    RECT* p = ap_data->r_list;  // 사각형 좌표 목록의 시작 주소를 얻는다.
-
-    // 포인터를 사용해서 저장된 사각형 좌표로 사각형을 하나씩 그린다.
-    for (int i = 0; i < ap_data->r_count; i++, p++) {
-        Rectangle(p->left, p->top, p->right, p->bottom);
-        //Rectangle(ap_data->r_list[i].left, ap_data->r_list[i].top,ap_data->r_list[i].right, ap_data->r_list[i].bottom);
-        //위 구문은 구조체를 배열구문으로 쓰고 for문을 돌게 되면, 결국은 숨겨진 +i가 4번 돌아가는 것이라 비효율적이다.
-        //따라서 RECT포인터를 하나 선언해서 
-    }
-}
-
-// 마우스 왼쪽 버튼이 눌렀다가 해제 되었을 때 호출되는 함수를 정의한다!
-void OnLButtonUp(int a_mixed_key, POINT a_pos)
-{
-    AppData* p_data = (AppData*)GetAppData(); // 프로그램의 내부 데이터 주소를 가져온다.
-
-    if (p_data->is_clicked == 1) { // 마우스가 클릭된 상태인지 체크한다.
-        p_data->is_clicked = 0; // 마우스를 클릭 해제 상태로 설정한다.
-        if (p_data->r_count < MAX_COUNT) { // 최대 저장 개수를 체크한다.
-            // 현재 시작 점과, 끝점을 사각형 목록에 추가한다.
-            p_data->r_list[p_data->r_count].left = p_data->start_pos.x;
-            p_data->r_list[p_data->r_count].top = p_data->start_pos.y;
-            p_data->r_list[p_data->r_count].right = a_pos.x;
-            p_data->r_list[p_data->r_count].bottom = a_pos.y;
-            p_data->r_count++;  // 저장된 사각형의 개수를 증가시킨다.
+    // 따라서 x는 0 ~ 5사이의 값을 가져야 유효한 값이고 y는 0만 유효한 값이 됩니다.
+    if (x < 6 && y < 1) {
+        // 6개의 사각형을 모두 어두운 파란색으로 출력합니다.
+        for (int i = 0; i < 6; i++) {
+            Rectangle(20 + i * 60, 60, 20 + (i + 1) * 60, 120, RGB(0, 100, 200), RGB(0, 0, 128));
         }
-        DrawRectList(p_data);  // 저장된 사각형을 다시 그린다.
-        ShowDisplay(); // 정보를 윈도우에 출력한다. 메시지 처리 함수(Handler)에서도 구성된 그림을 화면에 출력하기 위해 꼭 써줘야한다
-    }
-}
-
-// 마우스가 이동될 때마다 호출되는 함수를 정의한다!
-void OnMouseMove(int a_mixed_key, POINT a_pos)
-{
-    AppData* p_data = (AppData*)GetAppData(); // 프로그램의 내부 데이터 주소를 가져온다.
-
-    if (p_data->is_clicked == 1) {  // 마우스가 클릭된 상태인지 체크한다.
-        DrawRectList(p_data); // 저장된 사각형을 다시 그린다.
-        // 현재 편집중인 사각형을 그린다.
-        Rectangle(p_data->start_pos.x, p_data->start_pos.y, a_pos.x, a_pos.y);
+        // 사용자가 선택한 위치에 해당하는 사각형만 밝은 색 사각형으로 다시 그립니다.
+        Rectangle(20 + x * 60, 60, 20 + (x + 1) * 60, 120, RGB(0, 200, 255), RGB(0, 0, 255));
         ShowDisplay(); // 정보를 윈도우에 출력한다.
     }
 }
 
-// 마우스 왼쪽 버튼 누르거나, 해제하거나 이동할 때 호출될 함수들을 등록한다!
-MOUSE_MESSAGE(OnLButtonDown, OnLButtonUp, OnMouseMove)//바로 위 라인 주석대로 3가지를 적을 수 있다. 
+// 마우스 왼쪽 버튼 누를 때 호출될 함수를 등록한다!
+MOUSE_MESSAGE(OnLButtonDown, NULL, NULL)
 
 int main()
 {
-    AppData data = { {0, 0}, 0, 0 };  // 프로그램이 내부적으로 사용할 메모리를 선언한다.
-    SetAppData(&data, sizeof(AppData)); // 지정한 변수를 내부 데이터로 저장한다.
+    // (10, 10) 좌표에 파란색으로 안내 메시지를 출력한다!
+    TextOut(10, 10, RGB(0, 0, 255), "사각형을 클릭하면 색상이 변경됩니다~!");
 
-    SelectPenObject(RGB(0, 0, 255), 3);  // 파랑색이고 굵기가 3인 선 속성을 사용한다.
-    SelectStockObject(NULL_BRUSH);  // 사각형 내부를 채우지 않겠다고 설정한다.
+    // 수평 방향으로 6개의 파란색 사각형을 나열한다.
+    for (int i = 0; i < 6; i++) {
+        Rectangle(20 + i * 60, 60, 20 + (i + 1) * 60, 120, RGB(0, 100, 200), RGB(0, 0, 128));
+    }
 
     ShowDisplay(); // 정보를 윈도우에 출력한다.
     return 0;
