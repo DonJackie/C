4e6bc598064d749114128c8779c0ac2e2e5a3bd2
DonJackie <o12@kakao.com>
2022-12-15 16:31:48 +0900

    채팅 서버 만들기

--- ExamWin32.cpp
+++ ExamWin32.cpp
@@ -1,64 +1,78 @@
-﻿
-
-#include "pch.h"
+﻿#include "pch.h"
+#include <stdio.h>   // sprintf_s 함수를 사용하기 위해!!
 #include "tipsware.h"
 
-#define X_COUNT     19      // X축으로 그려질 줄의 개수
-#define Y_COUNT     19      // Y축으로 그려질 줄의 개수
-#define GRID_SIZE   20      // 사각형의 폭과 높이에 대한 크기
-
-struct AppData  // 프로그램에서 사용할 내부 데이터
+// 서버에 접속하는 사용자를 관리하기 위한 구조체!! (1명에 해당하는 정보만 구성)
+struct UserData
 {
-    char state[Y_COUNT][X_COUNT]; // 각 교차점의 상태(0:비어있음, 1:검은색 돌, 2:흰색 돌)
-    char step;  // 검은색 돌(0) 또는 흰색 돌(1)을 놓을 것인지 저장하는 변수
+    unsigned int h_socket;   // 소켓 핸들
+    char ip_address[16];     // 접속한 클라이언트의 주소
 };
 
-// 마우스 왼쪽 버튼이 눌러졌을 때 호출되는 함수를 정의한다!
-void OnLButtonDown(int a_mixed_key, POINT a_pos)
+// 새로운 클라이언트가 접속을 하면 이 함수가 호출됨!!
+// ap_user_data에는 접속한 클라이언트 정보가 이미 구성되어 있음!
+// ap_server에는 서버 객체에 대한 주소가 들어있음!
+void OnNewUser(UserData* ap_user_data, void* ap_server, int a_server_index)
 {
-    // 폭과 높이가 GRID_SIZE 크기인 격좌 좌표로 변환한다.
-    unsigned int x = a_pos.x / GRID_SIZE, y = a_pos.y / GRID_SIZE;
-    // 프로그램의 내부 데이터 주소를 가져온다. 2차원 배열의 주소를 받는다!
-    AppData* p_data = (AppData*)GetAppData();
-
-    // x는 0 ~ (X_COUNT-1) 사이의 값을 가져야 유효한 값이고
-    // y는 0 ~ (Y_COUNT-1) 사이의 값을 가져야 유효한 값입니다.
-    // 그리고 state[y][x] 값이 0인 경우에만 돌을 놓을수 있다
-    if (x < X_COUNT && y < Y_COUNT && !p_data->state[y][x]) {
-        // step 값이 0이면 state에 1을 대입하고 1이면 2를 대입한다.
-        // 흰돌 또는 검은 돌이 놓여졌음을 설정한다.
-        p_data->state[y][x] = p_data->step + 1;
-
-        // state 값에 따라 흰색 또는 검은색 원을 그리기 위해 색상을 테이블로 만든다.
-        COLORREF fill_color[2] = { RGB(0, 0, 0), RGB(255, 255, 255) };
-        // state[y][x]의 값이 1이면 검은색 돌을 2이면 흰색 돌을 그린다.
-        Ellipse(x * GRID_SIZE, y * GRID_SIZE, (x + 1) * GRID_SIZE + 1, (y + 1) * GRID_SIZE + 1,
-            RGB(0, 0, 0), fill_color[p_data->state[y][x] - 1]);
-
-        // step 값을 토글(0이면1, 1이면 0)한다.
-        p_data->step = !p_data->step;
-
-        ShowDisplay(); // 정보를 윈도우에 출력한다.
-    }
+    char temp_str[64];
+    sprintf_s(temp_str, 64, "새로운 사용자가 %s에서 접속을 했습니다!", ap_user_data->ip_address);
+    // 접속된 클라이언트 주소가 담긴 문자열을 리스트 박스(컨트롤 아이디 1000번)에 추가한다.
+    ListBox_InsertString(FindControl(1000), -1, temp_str);
 }
 
-// 마우스 왼쪽 버튼 누를 때 호출될 함수를 등록한다!
-MOUSE_MESSAGE(OnLButtonDown, NULL, NULL)
+// 클라이언트가 데이터를 전송하면 이 함수가 호출됨!!
+int OnClientMessage(CurrentServerNetworkData* ap_data, void* ap_server, int a_server_index)
+{
+    // 전달된 사용자 정보를 자신이 선언한 구조체로 형 변환해서 사용!
+    // 서버가 만들어질 때 Sizeof(UserData) 크기로 만들어 달라고 지정했기 때문에 내부적으로
+    // 사용자 정보는 UserData 형식으로 관리되고 있음
+    UserData* p_user_data = (UserData*)ap_data->mp_net_user;
+    char temp_str[128];
+    if (ap_data->m_net_msg_id == 1) { // 채팅 데이터가 전달됨 (1번 아이디는 개발자가 정한 것임)
+        // 누가 전송했는지 확인 하기 위해서 채팅을 전송한 클라이언트의 인터넷 주소를 채팅 데이터 앞에
+        // 붙여서 채팅 내용을 재구성한다.
+        sprintf_s(temp_str, 128, "%s : %s", p_user_data->ip_address, ap_data->mp_net_body_data);
+        // 재구성된 채팅 내용을 리스트 박스(컨트롤 아이디 1000번)에 추가한다.
+        ListBox_InsertString(FindControl(1000), -1, temp_str);
+        // 접속한 모든 클라이언트에게 채팅 내용을 다시 전송한다.
+        BroadcastFrameData(ap_server, 1, temp_str, strlen(temp_str) + 1);
+    }
+    return 1;
+}
+
+// 클라이언트가 접속을 해제하면 이 함수가 호출됨!!
+void OnCloseUser(UserData* ap_user_data, void* ap_server, int a_error_flag, int a_server_index)
+{
+    char temp_str[64];
+    if (a_error_flag == 1) {  // 오류로 인한 클라이언트 해제!!
+        sprintf_s(temp_str, 64, "%s에서 접속한 사용자를 강제로 접속 해제했습니다.", ap_user_data->ip_address);
+    }
+    else {  // 정상적인 클라이언트 해제!!
+        sprintf_s(temp_str, 64, "%s에서 사용자가 접속을 해제하였습니다!", ap_user_data->ip_address);
+    }
+    // 클라이언트의 해제 상태를 리스트 박스((컨트롤 아이디 1000번))에 추가한다.
+    ListBox_InsertString(FindControl(1000), -1, temp_str);
+}
+
+// 이 프로그램은 특별한 메시지를 사용하지 않는다!
+NOT_USE_MESSAGE
 
 int main()
 {
-    AppData data;  // 프로그램이 내부적으로 사용할 메모리를 선언한다.
-    memset(&data, 0, sizeof(AppData));  // data 변수의 값을 모두 0으로 초기화한다.
-    SetAppData(&data, sizeof(data));  // 지정한 변수를 내부 데이터로 저장한다.
+    ChangeWorkSize(620, 240); // 작업 영역을 설정한다.
+    Clear(0, RGB(72, 87, 114)); // 윈도우의 배경을 RGB(72, 87, 114) 색으로 채운다!
+    StartSocketSystem();  // 이 프로그램이 소켓 시스템을 사용하겠다고 설정한다.
 
-    // 바둑판을 그린다. 사각형을 18*18 = 324개를 그린다.
-    for (unsigned int y = 0; y < Y_COUNT - 1; y++) {
-        for (unsigned int x = 0; x < X_COUNT - 1; x++) {
-            Rectangle(GRID_SIZE / 2 + x * GRID_SIZE, GRID_SIZE / 2 + y * GRID_SIZE,
-                GRID_SIZE / 2 + (x + 1) * GRID_SIZE + 1, GRID_SIZE / 2 + (y + 1) * GRID_SIZE + 1,
-                RGB(0, 0, 0), RGB(244, 176, 77));
-        }
-    }
+    // UserData 구조체를 사용하는 서버를 생성한다. 이 서버는 자신의 상태에 따라 위에서 정의한
+    // OnNewUser, OnClientMessage, OnCloseUser 함수를 호출해서 작업을 진행한다.
+    void* p_server = CreateServerSocket(sizeof(UserData), OnNewUser, OnClientMessage, OnCloseUser);
+    // "127.0.0.1"에서 25001번 포트로 서버 서비스를 시작한다. (ip는 ipconfig)
+    StartListenService(p_server, "127.0.0.1", 25001);
+
+    CreateListBox(10, 30, 600, 200, 1000); // 1000번 아이디를 가진 리스트 박스를 생성한다.
+    SelectFontObject("굴림", 12); // 글꼴을 '굴림', 12 크기로 변경한다.
+    TextOut(15, 10, RGB(200, 255, 200), "사용자 채팅글 목록");  // 리스트 박스의 제목을 출력한다.
+
     ShowDisplay(); // 정보를 윈도우에 출력한다.
     return 0;
 }
